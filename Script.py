5cff6f8253fa51876a35bfb538bef3f5d11b7f52
Matecitos <31082992+Kevinmoth@users.noreply.github.com>
2023-09-03 13:53:39 -0300

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,8 +1,7 @@
-# @title Recorriendo Misiones
 import networkx as nx
 import matplotlib.pyplot as plt
 
-#Esta es una matriz de adyasencia,y es la forma matematica de representar un grafo
+# Esta es una matriz de adyacencia, y es la forma matemática de representar un grafo
 matriz_adyacencia = [
     [0, 28, 70, 83, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [28, 0, 0, 44, 0, 0, 73, 38, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -22,7 +21,8 @@ matriz_adyacencia = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 99, 0, 91],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 144, 151, 91, 0]
 ]
-#Le damos nombres a los nodos
+
+# Le damos nombres a los nodos
 nombres_nodos = {
     "Posadas": 0,
     "Candelaria": 1,
@@ -42,7 +42,8 @@ nombres_nodos = {
     "Iguazu": 15,
     "Gral.Belgrano": 16
 }
-# definimos la funcion que va a generar las aristas, ademas usando al libreria networkx, se calcula la distancia y la ruta ams corta:
+
+# Definimos la función que va a generar las aristas, además usando la librería networkx, se calcula la distancia y la ruta más corta:
 def encontrar_ruta_mas_corta(matriz, nodo_inicial, nodo_final):
     nodos = len(matriz)
     G = nx.Graph()
@@ -63,13 +64,18 @@ def encontrar_ruta_mas_corta(matriz, nodo_inicial, nodo_final):
 nodo_inicial = input("Ingresa el nodo o departamento inicial: ")
 nodo_final = input("Ingresa el nodo o departamento de destino: ")
 
-# LLamamos a nuestra funcion encontrar_ruta_mas_corta() , almacenamos los resultados y los mostramos en pantalla.
+# Convertir la primera letra de la entrada del usuario a mayúsculas y el resto a minúsculas
+nodo_inicial = nodo_inicial[0].upper() + nodo_inicial[1:].lower()
+nodo_final = nodo_final[0].upper() + nodo_final[1:].lower()
+
+# Llamamos a nuestra función encontrar_ruta_mas_corta(), almacenamos los resultados y los mostramos en pantalla.
 ruta_mas_corta, distancia_mas_corta = encontrar_ruta_mas_corta(matriz_adyacencia, nodo_inicial, nodo_final)
 
 print(f"Ruta más corta: {ruta_mas_corta}")
-print(f"Distancia más corta: {distancia_mas_corta} Kilometros")
+print(f"Distancia más corta: {distancia_mas_corta} Kilómetros")
 print(f"El viaje durará una media de: {(distancia_mas_corta)*60/6600} hs")
-# Con la libreria networkx, se dibuja el grafo
+
+# Con la librería networkx, se dibuja el grafo
 G = nx.Graph()
 
 for i in range(len(matriz_adyacencia)):
@@ -79,15 +85,33 @@ for i in range(len(matriz_adyacencia)):
                        list(nombres_nodos.keys())[list(nombres_nodos.values()).index(j)],
                        weight=matriz_adyacencia[i][j])
 
-pos = nx.spring_layout(G, seed=42)
+# Parámetros vectoriales para que el grafo tenga la forma del mapa
+pos = {
+    "Posadas": (31, -191),
+    "Candelaria": (133, -135),
+    "Apostoles": (86, -294),
+    "Alem": (203, -233),
+    "Concepcion": (96, -373),
+    "San Javier": (199, -330),
+    "Obera": (316, -247),
+    "San Ignacio": (255, -110),
+    "25 de Mayo": (445, -198),
+    "Cainguas": (434, -106),
+    "Gral S.Martin": (327, -29),
+    "Guarani": (590, -98),
+    "Montecarlo": (422, 15),
+    "San Pedro": (640, 16),
+    "El Dorado": (411, 86),
+    "Iguazu": (477, 173),
+    "Gral.Belgrano": (617, 146),
+}
 
 # Asigno los colores del grafo (rojo para la ruta más corta, gris para las demás)
-# mejorar colores*** (https://networkx.org/documentation/stable/auto_examples/drawing/plot_labels_and_colors.html)
-edge_colors = ['red' if (u, v) in zip(ruta_mas_corta, ruta_mas_corta[1:]) else 'gray' for u, v in G.edges()]
-edge_labels = {(u, v): d['weight'] for u, v, d in G.edges(data=True)}
+colores = ['red' if (u, v) in zip(ruta_mas_corta, ruta_mas_corta[1:]) else 'gray' for u, v in G.edges()]
+aristas = {(u, v): d['weight'] for u, v, d in G.edges(data=True)}
 
-# Mostramos en un mapa de vectores el grafico generado
-nx.draw(G, pos, with_labels=True, node_size=500, node_color='skyblue', font_size=8, edge_color=edge_colors)
-nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_size=6)
+# Mostramos en un mapa de vectores el gráfico generado
+nx.draw(G, pos, with_labels=True, node_size=500, node_color='skyblue', font_size=8, edge_color=colores)
+nx.draw_networkx_edge_labels(G, pos, edge_labels=aristas, font_size=6)
 plt.title("Recorrido a realizar en el grafo")
 plt.show()
